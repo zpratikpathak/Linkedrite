d20a316a08491a63f03afdce5cbc1e61e625881a
Pratik Pathak <pratikpathak200@gmail.com>
2024-04-21 19:32:32 +0530

    solved typo

--- rewrite/views.py
+++ rewrite/views.py
@@ -31,7 +31,7 @@ def rewrite(request):
             api_key=api_key, api_version=api_version, azure_endpoint=azure_endpoint
         )
 
-        prompt = "Consider yourself writing a linkedIn post now Rewrite the following text and make it more engaing and attractive. Correct the Grammar. It should have professional tone. The post is public, it should be in indirect speech. It should be clear and precise. Only return the rewritten text."
+        prompt = "Consider yourself writing a linkedIn post now Rewrite the following text and make it more engaging and attractive. Correct the Grammar. It should have professional tone. The post is public, it should be in indirect speech. It should be clear and precise. Only return the rewritten text."
         if data["emojiNeeded"]:
             prompt += " Add emojis to make it more engaging."
         prompt += "\n Now rewrite this text:" + data["postInput"]
