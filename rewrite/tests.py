df020e93c6d68395faea64ab32ee0d080a24c2bc
Pratik Pathak <pratikpathak200@gmail.com>
2024-05-25 09:59:40 +0530

    added testcases

--- rewrite/tests.py
+++ rewrite/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from .models import APICounter
 
-# Create your tests here.
+
+class RewriteAPITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse(
+            "rewrite"
+        )  # replace 'rewrite_api' with the actual name of the url in your urls.py
+
+    def test_post_request(self):
+        # Test a successful post request
+        response = self.client.post(
+            self.url,
+            {
+                "postInput": "This is a test post.",
+                "emojiNeeded": True,
+                "htagNeeded": True,
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["success"], True)
+
+        # Test a post request with a short postInput
+        response = self.client.post(
+            self.url, {"postInput": "Short", "emojiNeeded": True, "htagNeeded": True}
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["success"], False)
+        self.assertEqual(
+            response.data["message"], "The length of the post is too short."
+        )
+
+    def test_get_request(self):
+        # Test a get request
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["success"], False)
