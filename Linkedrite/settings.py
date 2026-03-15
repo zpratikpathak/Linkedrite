880c201a4e497b462860f804b8c6e959e910ee81
Pratik Pathak <pratikpathak200@gmail.com>
2026-03-15 23:08:52 +0530

    Add robust cleanup mechanisms for temporary files on interruption

--- Linkedrite/settings.py
+++ Linkedrite/settings.py
@@ -23,15 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@fazk2f(^ev@zy-*@m@&oj@tw2b77q8zu6fy&g8-2dl^h*xv$$"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 load_dotenv(".env")
-DEBUG = os.getenv("Debug")
-# print(DEBUG)
 
-ALLOWED_HOSTS = ["*"]
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-change-me-in-production")
+
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
